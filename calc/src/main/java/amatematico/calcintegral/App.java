e88562c043a7505844807a44fa403fe85ee62f55
Alex García <alejandrogarci2000@gmail.com>
2021-12-14 16:34:36 +0100

    + calcular por metodo trapecios

--- calc/src/main/java/amatematico/calcintegral/App.java
+++ calc/src/main/java/amatematico/calcintegral/App.java
@@ -9,15 +9,17 @@ public class App
 {
     static final String OPCION_RECTANGULOS = "R";
     static final String OPCION_EXACTA = "E";
+    static final String OPCION_TRAPECIOS = "T";
     static final String OPCION_SALIR = "S";
     static final String OPCION_DOC = "DOC";
     static final double MIN2_NOCERO = 0.000001;
     static Scanner sc;
+    
     public static void main( String[] args )
     {
 
         System.out.println("[S] INICIANDO CALCULADORA . . .");
-        System.out.println("Esta aplicacion usa la libreria mXparser, para mas informacion mira la seccion DOC");
+        System.out.println("Esta aplicación usa la librería mXparser, para mas información mira la seccion DOC");
         sc = new Scanner(System.in);
 
         String opcion = "";
@@ -41,15 +43,81 @@ public class App
                     System.out.println("--------------------------------------------------------------------------------");
                     mXparser.consolePrint(mXparser.LICENSE);
                     System.out.println("--------------------------------------------------------------------------------");
-                    System.out.println("---------------------------- GRUPO A. MATEMATICO -------------------------------");
+                    System.out.println("---------------------------- GRUPO A. MATEMÁTICO -------------------------------");
                     System.out.println("--------------------------------------------------------------------------------");
                     System.out.println("Trabajo realizado por . . . . . . . . . .");
                     break;
             
+                case OPCION_TRAPECIOS:
+                    resolverTrapecios();
+                    break;
                 default:
                     break;
             }
         }
+    }
+
+    private static void resolverTrapecios() {
+        Integral integral = readIntegral();
+
+        System.out.println("[S] Introduce el número de trapecios para utilizar:");
+        int nTrapecios = sc.nextInt();
+        sc.nextLine();
+
+        double ancho, area, fx1, fx2;
+        double p1, p2, aux;
+        double aTotal = 0;
+
+        Argument a = new Argument("x", 1);
+        Expression e;
+        
+        String fncX = integral.getParteIntegral().replaceAll("dx", "1");
+        e = new Expression(fncX, a);
+
+        ancho = (integral.getExtremoDcho()-integral.getExtremoIzqdo())/nTrapecios;
+
+        for (int i = 1; i <= nTrapecios; i++) {
+            //Sacamos el x1 y x2 para el trapecio 'i' en el que nos encontramos
+            p1 = (i-1) * ancho;
+            p2 = (i) * ancho;
+
+            //Calculamos la altura de cada uno de los lados, siendo f(xi-1) y f(xi) la izquierda
+            //y la derecha, respectivamente. Con el metodo checkCero, comprobamos si el valor es 0,
+            //que puede no existir la f(0) en algunas funciones, y se sustituye por 0.000001 para aproximarlo.
+            e.setArgumentValue("x", checkCero(p1));
+            fx1 = e.calculate();
+
+            e.setArgumentValue("x", checkCero(p2));
+            fx2 = e.calculate();
+
+            //Despejamos en la formula para hallar el area del trapecio, que es ancho * ((altura1 + altura2) / 2)
+            //Con el metodo ifNaN, evitamos que si la funcion no existe en ese punto, no funcione el metodo, sustituyendo
+            //el valor NaN de la fx por 0.
+            aux = ifNan(fx1)+ifNan(fx2);
+            area = ancho * (aux/2);
+
+            //Mostramos lo calculado para cada trapecio
+            DecimalFormat decimalFormat = new DecimalFormat("###0.0000");
+            DecimalFormat decimalFormat2 = new DecimalFormat("###0.0000000000");
+            DecimalFormat decimalFormat3 = new DecimalFormat("###000");
+            System.out.println("[INTEGRAL T" + decimalFormat3.format(i) +"] x" + (i-1) + "=" + decimalFormat.format(p1) + " f(x" + (i-1) + ")=" + decimalFormat2.format(fx1) + ", x" + i + "=" + decimalFormat.format(p2) + " f(x" + i + ")=" + decimalFormat2.format(fx2)+ " area=" + decimalFormat2.format(area));
+
+            //Si el area es un numero (existe) se suma al total
+            if (!Double.isNaN(area)) {
+                aTotal += area; 
+            }
+            System.out.print("(" + i + "/" + nTrapecios + ")\r");
+        }
+
+        System.out.println("\n[S] Resultado de "+ integral.toString() + ":");
+        if (aTotal == 0.0) {
+            System.out.println("[ERROR] Error a la hora de calcular f(x), verifica la función introducida.");
+        } else {
+            System.out.println("[RESULTADO INTEGRAL] " + aTotal);
+        }
+
+
+
     }
 
     private static void resolverExacta() {
@@ -58,6 +126,14 @@ public class App
         String eString = "int(" + fncX + ", x, " + checkCero(integral.getExtremoIzqdo()) + ", " + checkCero(integral.getExtremoDcho()) + ")";
         Expression e = new Expression(eString);
         System.out.println("[RESULTADO INTEGRAL] '"+ e.getExpressionString() + "': "+ e.calculate());
+    }
+
+    private static double ifNan(double d) {
+        if (Double.isNaN(d)) {
+            return 0;
+        } else {
+            return d;
+        }
     }
 
     private static double checkCero(double d) {
@@ -74,15 +150,12 @@ public class App
         "\n  Para introducir la funcion, debes tener en cuenta la nomenclatura" +
         "\n  estandar (sin,cos,...), la incognita debe ser 'x' y sin incluir" +
         "\n  extras como extremos, que se solicitaran mas tarde. Las raices" +
-        "\n  se representan como elevados a una fraccion '^(1/n)'");
+        "\n  cuadradas se representan como sqrt(INTERIOR)");
         
         Integral integral = readIntegral();
-        
-        //Inicializamos el resultado de f(x)
-        double fx = 0;
 
         //Inicializamos las variables fuera del bucle
-        double ancho, area;
+        double ancho, area, fx;
         double aTotal = 0;
 
         //Argument sera la clase que evalue la X a sustituir en la expresion introducida,
@@ -91,11 +164,11 @@ public class App
         Argument x;
         Expression e;
 
-        System.out.println("[S] Introduce el numero de rectangulos para utilizar:");
+        System.out.println("[S] Introduce el número de rectángulos para utilizar:");
         int nRectangulos = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("[S] CALCULANDO LA INTEGRAL '" + integral.getParteIntegral() + "' (METODO DE LOS RECTANGULOS)");
+        System.out.println("[S] CALCULANDO LA INTEGRAL '" + integral.getParteIntegral() + "' (MÉTODO DE LOS RECTÁNGULOS)");
 
         //Sacamos el ancho de base de cada rectangulo, que seria igual al extremo derecho
         //menos el extremo izquiero dividido entre el numero de rectangulos
@@ -154,7 +227,7 @@ public class App
 
         System.out.println("\n[S] Resultado de "+ integral.toString() + ":");
         if (aTotal == 0.0) {
-            System.out.println("[ERROR] Error a la hora de calcular f(x), verifica la funcion introducida.");
+            System.out.println("[ERROR] Error a la hora de calcular f(x), verifica la función introducida.");
         } else {
             System.out.println("[RESULTADO INTEGRAL] " + aTotal);
         }
@@ -176,11 +249,12 @@ public class App
     }
 
     private static String getOpcion() {
-        System.out.println("\n[MENU] Selecciona una opcion:");
-        System.out.println("  " + OPCION_RECTANGULOS + " - Calcular integral por metodo del rectangulo");
+        System.out.println("\n[MENU] Selecciona una opción:");
+        System.out.println("  " + OPCION_RECTANGULOS + " - Calcular integral por método del rectángulo");
         System.out.println("  " + OPCION_EXACTA + " - Calcular integral exacta");
-        System.out.println("  " + OPCION_DOC + " - Documentacion y licencia mXparser");
-        System.out.println("  " + OPCION_SALIR + " - Salir de la aplicacion");
+        System.out.println("  " + OPCION_TRAPECIOS + " - Calcular integral por el método de los trapecios");
+        System.out.println("  " + OPCION_DOC + " - Documentación y licencia mXparser");
+        System.out.println("  " + OPCION_SALIR + " - Salir de la aplicación");
         String res = sc.nextLine().toUpperCase();
         return res;
     }
--- exec.bat
+++ exec.bat
@@ -1,3 +1,3 @@
 @echo off
 title Calculadora de Matrices [JAVA]
-java -jar TrabajoT1RectangulomXparser.jar
+java -jar TrabajoT1RectanguloOtro.jar
